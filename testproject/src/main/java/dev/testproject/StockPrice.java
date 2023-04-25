3e75c0c59551050af712fcce1a067ffa1dc13e88
Max Franklin <mifrank1@mail.yu.edu>
2023-04-25 18:23:17 -0400

    Update StockPrice.java
    Added description to what is being imported.

--- testproject/src/main/java/dev/testproject/StockPrice.java
+++ testproject/src/main/java/dev/testproject/StockPrice.java
@@ -2,6 +2,22 @@ package dev.testproject;
 
 import java.io.*;
 import java.net.*;
+/*
+ * Sure! Here's a brief description of each of the classes being imported in the example:
+- `java.io.BufferedReader`: This class reads text from a character-input stream, buffering characters
+so as to provide for the efficient reading of characters, arrays, and lines.
+
+- `java.io.IOException`: This class is an exception that signals that an I/O exception of some sort has occurred.
+This class is the general class of exceptions produced by failed or interrupted I/O operations.
+
+- `java.io.InputStreamReader`: This class is a bridge from byte streams to character streams.
+It reads bytes and decodes them into characters using a specified charset.
+
+- `java.net.HttpURLConnection`: This class is a subclass of `URLConnection` that provides support for
+HTTP-specific features. Each instance of this class represents a single HTTP connection to a remote resource.
+
+- `java.net.URL`: This class represents a Uniform Resource Locator, a pointer to a "resource" on the World Wide Web.
+*/
 
 public class StockPrice {
     public static void main(String[] args) throws IOException {
